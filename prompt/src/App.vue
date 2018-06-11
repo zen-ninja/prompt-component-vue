e035a34786f9f7448c4e5da5a04245858aaec6d7
Zen-Ninja <ninja@zen-ninja.co.za>
2018-06-11 10:57:04 +0200

    wip: dynamic images and colors
    work in progress: setting up styling object for each item for dynamic switching between icon and color

--- prompt/src/App.vue
+++ prompt/src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <prompt></prompt>
+    <prompt v-for="_prompt in prompt" v-bind:showPrompt="showPrompt" v-bind:key="_prompt.id" v-bind:icon="_prompt.icon" v-bind:color="_prompt.color" v-bind:msg="_prompt.title" v-bind:info="_prompt.info" v-bind:btn="_prompt.simpleContent"></prompt>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  // data() {
-  //   const BUTTON_EL = document.querySelector(".btn.bg-blue");
-  //   const MESSAGE_EL = document.querySelector(".msg");
-  //   const MESSAGE_TITLE = document.querySelector(".msg h2");
-  //   const ICON_EL = document.querySelector(".svg");
-  //   const INFO_EL = document.querySelector(".info");
-  //   const INFO_PARAGRAPH = document.querySelector(".info p");
-  //   const PROMPT_EL = document.querySelector(".prompt-wrapper");
-  // }
+  data: function() {
+    var showPrompt = false
+    return {
+      prompt: [
+        {id: 1, simpleContent: false, icon: 'img/graphic-future-targets.svg', title: 'Target updated!'},
+        {id: 2, simpleContent: false, icon: 'img/graphic-stream-editor.svg', title: 'Task updated!'},
+        {id: 3, simpleContent: false, icon: 'img/icon-cross-circle.svg', color: '#93CC32', title: 'Changes saved.'},
+        {id: 4, simpleContent: true, icon: 'img/icon-exclamation-circle.svg', color: '#ED4444', title: 'Please note', info: 'The values you have entered as future targets will be deleted if you change the frequency of this promise.'},
+        {id: 5, simpleContent: true, icon: 'img/icon-line-circle.svg', title: 'Future Targets', info: "Values must be numeric, no more than 13 characters and cannot go beyond the target’s end date."},
+        {id: 6, simpleContent: true, icon: 'img/icon-tick-circle.svg', color: '#13AFD7', title: 'Stream Editors', info: 'Editors can add and remove goals and members, and can change the title and report frequency.'}
+      ],
+      showPrompt
+    }
+  },
+  mounted(){
+    this.cyclePrompts()
+  },
+  methods: {
+    togglePrompt: function() {
+      this.showPrompt = true;
+    },
+    cyclePrompts: function() {
+      for (var el=0;el<=6;el++) {
+        setTimeout(function(){
+          this.togglePrompt
+        }, 3000);
+      }
+    }
+  }
 }
 </script>
 
@@ -28,5 +48,116 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.u-flex-1 {
+  flex: 1;
+}
+
+.u-flex-v {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.u-flex-h {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn {
+  padding: 1em 0;
+  width: 100%;
+}
+
+.bg-blue {
+  background-color: #13AFD7;
+}
+
+.u-text-uppercase {
+  text-transform: uppercase;
+}
+
+.u-text-white {
+  color: white;
+}
+
+.is-hidden, .u-hidden {
+  display: none !important;
+}
+
+.svg {
+  width: 5em;
+  height: 5em;
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
+
+.svg.color-red {
+  fill: red;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+.prompt-wrapper {
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.8);
+  position: absolute;
+  z-index: 999;
+  top: 0;
+  left: 0;
+}
+
+.prompt-wrapper .prompt_content {
+  background-color: #fff;
+  border-radius: 5px;
+  width: 95%;
+  height: 95%;
+}
+
+@media screen and (min-width: 1024px) {
+  .prompt-wrapper .prompt_content {
+    width: 95%;
+    height: 90%;
+  }
+}
+
+.item {
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.item:hover {
+  filter: brightness(110%);
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 5vh;
+  background-color: #484848;
+  color: white;
+}
+
+.icon {
+  width: 2em;
+  height: 2em;
+  background-color: red;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+}
+
+section {
+  padding: 1em;
 }
 </style>
--- prompt/src/components/Prompt.vue
+++ prompt/src/components/Prompt.vue
@@ -1,113 +1,32 @@
 <template>
-  <div class="prompt-wrapper u-flex-h" v-bind:class="{ activatePrompt: 'is-hidden' }">
+  <div class="prompt-wrapper u-flex-h" v-bind:class="{ 'is-ahidden': !showPrompt }">
     <div class="prompt_content u-flex-v">
       <div class="u-flex-v u-flex-1">
-        <div class="svg">{{ icon }}</div>
+        <div class="svg" v-bind:style="{fill:+color+';','background-image':'url( /static/' +icon+')'}"></div>
         <div class="msg"><h2>{{ msg }}</h2></div>
         <div class="info"><p>{{ info }}</p></div>
       </div>
-      <div v-on:click="closePrompt()" class="btn bg-blue u-flex-h u-text-uppercase u-text-white" v-bind:class="{ simpleContent: 'is-hidden' }">Got it</div>
+      <div v-on:click="closePrompt()" class="btn bg-blue u-flex-h u-text-uppercase u-text-white" v-bind:class="{ 'is-hidden': simpleContent }">Got it</div>
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "Prompt",
-  // data() {
-  //   var icon,
-  //     msg,
-  //     info,
-  //     color = "#000",
-  //     activePrompt = false;
-  // },
-  props: ['icon', 'msg', 'info', 'color', 'activePrompt' ],
+  props: ['icon', 'msg', 'info', 'color'],
+  data: function() {
+    var showPrompt = false, simpleContent = false
+    var svgStyle = {
+      color: '',
+      img: ''
+    }
+    return {
+      showPrompt, simpleContent, svgStyle
+    }
+  },
   methods: {
-    promptUser: function(msg) {
-      activatePrompt();
-      if (
-        msg === "Target updated!" ||
-        msg === "Task updated!" ||
-        msg === "Changes saved."
-      ) {
-        simpleContent();
-      }
-
-      if (msg === "Target updated!" || msg === "Task updated!") {
-        stuffUpdated();
-      }
-
-      if (msg === "Changes saved.") {
-        changesSaved();
-      }
-
-      if (msg === "Please note") {
-        note();
-      }
-
-      if (msg === "Future Targets") {
-        targets();
-      }
-
-      if (msg === "Stream Editors") {
-        editors();
-      }
-    },
-    activatePrompt: function() {
-      if (PROMPT_EL.classList.contains("is-hidden")) {
-        PROMPT_EL.classList.remove("is-hidden");
-        activePrompt = true;
-      } else {
-        activePrompt = false;
-      }
-    },
-    simpleContent: function() {
-      if (
-        !BUTTON_EL.classList.contains("is-hidden") &&
-        MESSAGE_EL.classList.contains("is-hidden")
-      ) {
-        BUTTON_EL.classList.add("is-hidden");
-        MESSAGE_EL.classList.add("is-hidden");
-      }
-    },
-    stuffUpdated: function() {
-      icon = "icon-tick-circle";
-      color = "#93CC32";
-      insertContent(icon, msg, color, info);
-    },
-    editors: function() {
-      icon = "graphic-stream-editor";
-      msg =
-        "Editors can add and remove goals and members, and can change the title and report frequency.";
-      insertContent(icon, msg, color, info);
-    },
-    targets: function() {
-      icon = "graphic-future-targets";
-      msg =
-        "Values must be numeric, no more than 13 characters and cannot go beyond the target’s end date.";
-      insertContent(icon, msg, color, info);
-    },
-    note: function() {
-      icon = "icon-exclamation-circle";
-      msg =
-        "The values you have entered as future targets will be deleted if you change the frequency of this promise.";
-      color = "#ED4444";
-      insertContent(icon, msg, color, info);
-    },
-    changesSaved: function() {
-      icon = "icon-tick-circle";
-      color = "#13AFD7";
-      insertContent(icon, msg, color, info);
-    },
     closePrompt: function() {
-      if (!PROMPT_EL.classList.contains("is-hidden")) {
-        PROMPT_EL.classList.add("is-hidden");
-      }
-    },
-    insertContent: function(icon, msg, color, info) {
-      ICON_EL.classList.add(icon);
-      ICON_EL.classList.add("color-red");
-      MESSAGE_TITLE.innerHTML = msg;
-      INFO_PARAGRAPH.innerHTML = info;
+      this.showPrompt = false;
     }
   }
 };
